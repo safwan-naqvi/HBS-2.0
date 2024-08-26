56d49b2f6106eeecf787758502418ee4edcd0ce8
Syed Safwan Abbas <safwanabbas2020@gmail.com>
2024-08-26 16:28:21 +0500

    Strapi Added

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,16 @@ const nextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
     ],
   },
 };
--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,8 +1,51 @@
+import qs from "qs";
+
+import { flattenAttributes, getStrapiURL } from "@/lib/utils";
 import axios from "axios"
+// import { getAuthToken } from "./services/get-token";
+
+const baseUrl = getStrapiURL();
+
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     headers: {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_BACKEND_API_KEY}`
     }
 })
+
 export default api
+// async function fetchData(url: string) {
+//     const authToken = await getAuthToken();
+
+//     const headers = {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${authToken}`,
+//         },
+//     };
+
+//     try {
+//         const response = await fetch(url, authToken ? headers : {});
+//         const data = await response.json();
+//         return flattenAttributes(data);
+//     } catch (error) {
+//         console.error("Error fetching data:", error);
+//         throw error; // or return null;
+//     }
+// }
+
+// async function getStrapiData(path: string, pageQuery: string) {
+//     const baseUrl = "http://localhost:1337";
+
+//     const url = new URL(path, baseUrl);
+//     url.search = pageQuery;
+
+//     try {
+//         const response = await fetch(url.href);
+//         const data = await response.json();
+//         return data;
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
