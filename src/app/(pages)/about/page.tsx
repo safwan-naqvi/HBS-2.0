ffc1f7329c653047b15f7ff34db7df131b86f008
Syed Safwan Abbas <safwanabbas2020@gmail.com>
2024-09-24 15:18:25 +0500

    Lenis Prevent Added in About

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -30,7 +30,7 @@ export default function About() {
         {/* <Client/>  */}
         {/* Careers  */}
       </div>
-      <div className="relative grid grid-cols-1 md:grid-cols-2 w-full px-10 lg:px-20 text-white pt-40 pb-40 bg-[#B5C0C9]">
+      <div className="relative grid grid-cols-1 md:grid-cols-2 w-full px-10 lg:px-20 text-white pt-40 pb-40 bg-[#B5C0C9]" data-lenis-prevent>
         <h2 className="text-[#111] text-3xl md:text-7xl font-semibold">Have a Project Idea?</h2>
         <img src="/assets/contact_bg.webp" alt="Contact Image" className="object-cover absolute bottom-0 left-0" />
 
